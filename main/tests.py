dab36458aef7c71965d0191890c2edaa3157109b
benyoss <benyoss4@gmail.com>
2021-04-15 23:06:50 -0500

    (fix) main render via reinstall

--- main/tests.py
+++ main/tests.py
@@ -1,13 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-class CtrlFARTestCase(TestCase):
-    def tests_work(self):
-        """Test to see if tests work in project"""
-        red = 'red'
-        blue = 'blue'
-        yellow = 'yellow'
-        black = False
-        if (red and blue and yellow):
-            black = True
-        self.assertEqual(black, True)
